e2d84d938d375fdb6005b2d318f50b22fbc65d1f
Mingyu Kim <mingyu.kim@knu.ac.kr>
2026-02-07 15:46:04 +0900

    video view open command - file dialog

--- TutorialApp/ViewModel/VideoViewModel.cs
+++ TutorialApp/ViewModel/VideoViewModel.cs
@@ -6,7 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 using LibVLCSharp.Shared;
+using Microsoft.Win32;
 
 namespace TutorialApp.ViewModel
 {
@@ -16,6 +19,7 @@ namespace TutorialApp.ViewModel
 
         [ObservableProperty]
         private ObservableCollection<MediaPlayer> _mediaPlayers = [];
+        private bool _isFileDialogOpened;
 
         public VideoViewModel()
         {
@@ -28,6 +32,53 @@ namespace TutorialApp.ViewModel
 
                 MediaPlayers.Add(player);
             }
+            _isFileDialogOpened = false;
+        }
+
+        [RelayCommand]
+        private async Task Open(MediaPlayer player)
+        {
+            if (_isFileDialogOpened)
+            {
+                return;
+            }
+
+            _isFileDialogOpened = true;
+
+            try
+            {
+                OpenFileDialog openFileDialog = new();
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    await Task.Run(() =>
+                    {
+                        var media = new Media(_libVLC, new Uri(filePath));
+
+                        media.AddOption(":input-repeat=1");
+
+                        player.Play(media);
+                    });
+                }
+            }
+            finally
+            {
+                _isFileDialogOpened = false;
+            }
+        }
+
+        [RelayCommand]
+        private void Play(MediaPlayer player)
+        {
+
+        }
+
+        [RelayCommand]
+        private void Stop(MediaPlayer player)
+        {
+
         }
     }
 }
